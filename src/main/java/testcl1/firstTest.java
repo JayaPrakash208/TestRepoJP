5bd8c71feb4b185d0a4b1e71659dad9dfc9b3f8f
jp_git <jayaprakashnarayaan208@gmail.com>
2024-02-10 17:14:44 +0530

    Origin

---
+++ src/main/java/testcl1/firstTest.java
@@ -0,0 +1,21 @@
+package testcl1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class firstTest {
+
+	@Test
+	public static void lauchbrowser() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://github.com/");
+		System.out.println("browser launched");
+
+	}
+
+}
