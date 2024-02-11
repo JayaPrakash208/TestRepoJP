753e39fae850f3d50a4c993767552bbf3b464257
jp_git <jayaprakashnarayaan208@gmail.com>
2024-02-11 22:02:00 +0530

    JPN-158

--- src/main/java/testcl1/SecondClass2.java
+++ src/main/java/testcl1/SecondClass2.java
@@ -1,8 +1,11 @@
 package testcl1;
 
+import org.testng.annotations.Test;
+
 public class SecondClass2 {
 
-	public static void main(String[] args) {
+	
+	public void main(String[] args) {
 System.out.println("Hello");
 	}
 
