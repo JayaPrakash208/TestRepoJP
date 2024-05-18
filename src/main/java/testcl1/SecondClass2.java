f33a102dc2a1e2b64fba5b5ca897fd505edd797f
jp_git <jayaprakashnarayaan208@gmail.com>
2024-05-18 19:07:04 +0530

    Updated Clear Chrome driver cache

--- src/main/java/testcl1/SecondClass2.java
+++ src/main/java/testcl1/SecondClass2.java
@@ -1,12 +1,19 @@
 package testcl1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SecondClass2 {
 
 	
-	public void main(String[] args) {
-System.out.println("Hello");
-	}
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().clearDriverCache().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://github.com/");
+		System.out.println("browser launched");	}
 
 }
--- src/main/java/testcl1/firstTest.java
+++ src/main/java/testcl1/firstTest.java
@@ -3,7 +3,6 @@ package testcl1;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class firstTest {
@@ -12,10 +11,7 @@ public class firstTest {
 	public static void lauchbrowser() {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		
 		driver.get("https://github.com/");
 		System.out.println("browser launched");
-
 	}
-
 }
